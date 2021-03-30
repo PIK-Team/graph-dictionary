981c678fbaf132d7d52ce2ded7261126c2d11a26
Paweł Budniak <45400182+PawelBudniak@users.noreply.github.com>
2021-03-30 18:01:51 +0200

    add SpringBootServletInitializer2

--- api/src/main/java/backend/SpringApp.java
+++ api/src/main/java/backend/SpringApp.java
@@ -3,12 +3,15 @@ package backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 
 import java.util.Collections;
 
 @SpringBootApplication
 @EnableNeo4jRepositories
-public class SpringApp {
+public class SpringApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SpringApp.class);
@@ -16,3 +19,4 @@ public class SpringApp {
         app.run(args);
     }
 }
+
