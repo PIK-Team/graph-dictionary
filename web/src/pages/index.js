dd3db55047360ffc7c8c1851a1e343b21f28ce78
Maciej Adamski <madamskip1@gmail.com>
2021-05-14 22:22:42 +0200

    Szablon: Header

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,10 +1,13 @@
 import React from "react"
 import Container from "../components/container"
 import { graphql } from 'gatsby';
+import Header from '../components/header'
+
 
 const HomePage = ({data}) => {
   return (
     <Container>
+		<Header></Header>
       <h1>Graph Dictionary</h1>
       Rignt now we have a total of <b> {data.apiPeople.page.totalElements} </b> elements in our database :)
     </Container>
