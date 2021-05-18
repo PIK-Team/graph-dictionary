d43bc2bcf1f470de6d8f031b4072a53ca6eb4ef3
Piotr Frątczak <piotrfratczak99@gmail.com>
2021-05-18 13:52:43 +0200

    Finish dictionary list view #PIK-46
    * Make list scrollable.
    
    * Get rid of underlines.
    
    Change list row color on hover

--- web/src/pages/dictionarylist.js
+++ web/src/pages/dictionarylist.js
@@ -18,7 +18,37 @@ export default class DictionaryList extends React.Component {
         {
             name: "Słownik sportowy",
             description: "Truskawka na torcie itd.",
-            logoUrl: "https://bi.im-g.pl/im/ff/72/15/z22487807IER,Truskawka-na-torcie.jpg"
+            logoUrl: "https://bi.im-g.pl/im/dc/6c/15/z22463964IER,Tomasz-Hajto.jpg"
+        },
+        {
+            name: "Słownik języka włoskiego",
+            description: "Parliamo italiano!",
+            logoUrl: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/658A0A74-039A-487C-A07A-CAAF61B4615D/Derivates/A230DF28-60DF-429D-ABDA-96ED64E9EE10.jpg"
+        },
+        {
+            name: "Słownik medyczny",
+            description: "Słownik terminów medycznych.",
+            logoUrl: "https://euro-bion.pl/wp-content/uploads/2020/08/AdobeStock_120965550.jpeg"
+        },
+        {
+            name: "Słownik sportowy",
+            description: "Truskawka na torcie itd.",
+            logoUrl: "https://bi.im-g.pl/im/dc/6c/15/z22463964IER,Tomasz-Hajto.jpg"
+        },
+        {
+            name: "Słownik języka włoskiego",
+            description: "Parliamo italiano!",
+            logoUrl: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/658A0A74-039A-487C-A07A-CAAF61B4615D/Derivates/A230DF28-60DF-429D-ABDA-96ED64E9EE10.jpg"
+        },
+        {
+            name: "Słownik medyczny",
+            description: "Słownik terminów medycznych.",
+            logoUrl: "https://euro-bion.pl/wp-content/uploads/2020/08/AdobeStock_120965550.jpeg"
+        },
+        {
+            name: "Słownik sportowy",
+            description: "Truskawka na torcie itd.",
+            logoUrl: "https://bi.im-g.pl/im/dc/6c/15/z22463964IER,Tomasz-Hajto.jpg"
         },
         {
             name: "Słownik języka włoskiego",
@@ -37,12 +67,10 @@ export default class DictionaryList extends React.Component {
                     <div className={dictionaryListStyle.list}>
                         {this.dictionarylist.map(dictionary => (
                             <Link to="#">
-                                <div className={dictionaryListStyle.overlay}>
-                                    <div className={dictionaryListStyle.row}>
-                                        <img src={dictionary.logoUrl} alt="logo" width="120" height="120"></img>
-                                        <div>{dictionary.description}</div>
-                                        <div className={dictionaryListStyle.dictName}>{dictionary.name}</div>
-                                    </div>
+                                <div className={dictionaryListStyle.row}>
+                                    <img src={dictionary.logoUrl} alt="logo" width="120" height="120"></img>
+                                    <div>{dictionary.description}</div>
+                                    <div className={dictionaryListStyle.dictName}>{dictionary.name}</div>
                                 </div>
                             </Link>
                         ))}
