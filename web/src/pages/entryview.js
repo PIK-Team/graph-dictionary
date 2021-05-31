bc20872a6edf300e6ae2c23f8c738f24e27738e9
Maciej Adamski <madamskip1@gmail.com>
2021-05-31 02:03:12 +0200

    style entry tree

--- web/src/pages/entryview.js
+++ web/src/pages/entryview.js
@@ -10,6 +10,7 @@ import MainWrapper from '../components/mainwrapper'
 import * as entryViewStyle from '../styles/entryview.module.css'
 import * as indexStyle from '../styles/index.module.css'
 import 'react-tree-graph/dist/style.css'
+import '../styles/entryTree.css'
 
 
 const showEntries = (entries, dictionary, thisWord) => {
@@ -25,20 +26,26 @@ const showEntries = (entries, dictionary, thisWord) => {
 }
 
 const getThisEntryInfo = (entries, thisWord) => {
-	console.log(entries)
-	console.log(entries[0])
-	return (
-		 entries[0].word == thisWord ? <div> <div className={entryViewStyle.defName}>Nazwa wpisu:
-							<span style={{fontWeight: "bold"}}> {entries[0].word}</span>
-						</div>
-						<div> Definicje:
-							<ul>
-							{ entries[0].definitions.map(definitionObj => (
-								<li> {definitionObj.definition} </li>
-							))}
-							</ul>
-						</div> </div> : getThisEntryInfo(entries[0].subentries, thisWord) 
-	)
+	if (entries[0] != undefined)
+	{
+		if (entries[0].word == thisWord)
+		{
+			return (
+				<div> <div className={entryViewStyle.defName}>Nazwa wpisu:
+					<span style={{fontWeight: "bold"}}> {entries[0].word}</span>
+					</div>
+					<div> Definicje:
+						<ul>
+						{ entries[0].definitions.map(definitionObj => (
+							<li> {definitionObj.definition} </li>
+						))}
+						</ul>
+				</div> </div>
+			)
+		}
+		
+		return getThisEntryInfo(entries[0].subentries, thisWord)
+	}
 }
 
 const simplifyEntriesArray = (entries) =>
@@ -48,33 +55,89 @@ const simplifyEntriesArray = (entries) =>
 	return entries
 }
 
+const prepareDataForTree = (entries, thisWord, thisDictionary) =>
+{
+	{ entries.map(entry => {
+		if (entry.word == thisWord)
+		{
+			entry.gProps = { className:  "node treeWord treeThisWord", 
+							onClick: function (event, nodeKey) 
+									{
+										window.location.href = `?dictionary=${ thisDictionary }&entry=${ nodeKey }`
+									}
+							}
+		}
+		else
+		{
+			entry.gProps = { className: "node treeWord", 
+							onClick: function (event, nodeKey) 
+										{
+											window.location.href = `?dictionary=${ thisDictionary }&entry=${ nodeKey }`
+										}
+							}
+		}
+		
+		prepareDataForTree(entry.subentries, thisWord, thisDictionary)
+	}) }
+}
+
+const calcWidthTree = (data) =>
+{
+	if (data.subentries.length == 0)
+	{
+		return 100
+	}
+	
+	let width = calcWidthTree(data.subentries[0])
+	width = width + 100
+	return width
+}
+
+const calcHeightTree = (data) =>
+{
+	if (data[0].subentries.length == 0)
+	{
+		let height = 0
+		data.map(entry => {
+			height = height + 1
+		})
+		height = height * 60
+		return height
+	}
+	
+	return calcHeightTree(data[0].subentries)
+}
+
 export default class EntryView extends React.Component {
 	
 	values = queryString.parse(this.props.location.search)
 	
-	dictionaryParam = this.values.dictionary
-	entryParam = this.values.entry
-	
 	state = {
+		dictionaryParam: this.values.dictionary,
+		entryParam: this.values.entry,
 		entry: null
 	}
 	
 	
-
 	componentDidMount() {
-		fetch(process.env.API_URL+'entries/'+this.dictionaryParam+'/'+this.entryParam+'/overview', {
+		fetch(process.env.API_URL+'entries/'+this.state.dictionaryParam+'/'+this.state.entryParam+'/overview', {
 			method: 'GET',
 		})
 		.then(response => response.json())
-		.then(json => simplifyEntriesArray(json))
-		.then(json => this.setState({entry:json}));
+		.then(json => {
+			simplifyEntriesArray(json)
+			prepareDataForTree([json], this.state.entryParam, this.state.dictionaryParam)
+			
+			return json
+		})
+		.then(json => this.setState({entry:json}))
 	}
 	
 	
 	render() {
-		const { entry } = this.state
+		const {entry} = this.state
 		
-		if ( this.entryParam === undefined || this.dictionaryParam === undefined)
+		if ( this.state.entryParam === undefined || this.state.dictionaryParam === undefined)
 		{
 			return (
 				<Container>
@@ -93,17 +156,14 @@ export default class EntryView extends React.Component {
 
 			)
 		}
-		
+
 		if ( entry === null) { 
-		
 			return (
 				<Container>
 					<Header></Header>
 					<SubpageHeader subpageName="Wpis w słowniku"></SubpageHeader>
 					<MainWrapper>
 					<div className={entryViewStyle.entryWrapper}>
-						{console.log("Test")}
-						{console.log(this.entryParam)}
 						ŁADOWANIE
 						
 					</div>
@@ -116,34 +176,36 @@ export default class EntryView extends React.Component {
 		}
 		
 		return(
+			
 			<Container>
 				<Header></Header>
 				<SubpageHeader subpageName="Wpis w słowniku"></SubpageHeader>
 				<MainWrapper>
-				<div className={entryViewStyle.entryWrapper}>
-					<div className={entryViewStyle.entryInfo, entryViewStyle.entryWrapperElement}>
-					
-							{ getThisEntryInfo([this.state.entry], this.entryParam) }
+					<div className={entryViewStyle.entryWrapper}>
+						<div className={entryViewStyle.entryInfo, entryViewStyle.entryWrapperElement}>
+
+								{ getThisEntryInfo([this.state.entry], this.state.entryParam) }
 							
-						<div className={indexStyle.indexButtonDiv}><Link to={`/newentry?dictionary=${this.dictionaryParam}&parent=${this.entryParam}`}  style={{width: "80%", fontSize: "10pt"}} className={indexStyle.indexButton}>Dodaj wpis potomny</Link></div>
-						
+						</div>
+						<div className={entryViewStyle.entryTree, entryViewStyle.entryWrapperElement}>
+									
+							<div className={indexStyle.indexButtonDiv}><Link to={`/newentry?dictionary=${this.state.dictionaryParam}&parent=${this.state.entryParam}`}  style={{width: "80%", fontSize: "10pt"}} className={indexStyle.indexButton}>Dodaj wpis potomny</Link></div>
+							
+						</div>
 					</div>
-					<div className={entryViewStyle.entryTree, entryViewStyle.entryWrapperElement}>
-						<div style={{marginBottom: "25px"}}> Drzewo wpisu: </div>
-						
-								{showEntries([this.state.entry], this.dictionaryParam, this.entryParam) }
-								
-								{console.log(this.state.entry)}
-								<Tree data={this.state.entry}
-										height={400}
-										width={400}
+					<div style={{textAlign: "center", marginTop: "50px", marginBottom: "25px"}}> Drzewo wpisu: </div>
+					<div style={{textAlign: "center"}}>
+						<Tree data={this.state.entry}
+										height={ calcHeightTree([this.state.entry]) }
+										width={ calcWidthTree(this.state.entry) + 200 }
 										getChildren={ node => node.subentries }
 										keyProp={"word"}
 										labelProp={"word"}
+										textProps = {{ dx: 10 }}
+									
 								/>
-						
 					</div>
-				</div>
+					<div style= {{ paddingBottom: "300px" }}></div>
 				</MainWrapper>
 				<Footer></Footer>
 			</Container>
